144a8c984fedff8b3903912e8240703fc752ca19
강한길 <59948163+gksrlf@users.noreply.github.com>
2022-09-07 15:39:30 +0900

    db연결

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const path = require("path")
 const app = express()
+const db_config = require(__dirname + "/config/database.js")
+const conn = db_config.init()
 
 app.listen(8001, function () {
 	console.log("listening on 8001")
@@ -8,6 +10,16 @@ app.listen(8001, function () {
 
 app.use(express.static(path.join(__dirname, "chat/build")))
 
-app.get("/main", (req, res) => {
+app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/chat/build/index.html"))
+})
+app.get("/test", (req, res) => {
+	conn.query(
+		"SELECT * FROM `memberQuestion`",
+		function (error, results, fields) {
+			if (error) throw error
+			res.json(results)
+			res.end()
+		}
+	)
 })
