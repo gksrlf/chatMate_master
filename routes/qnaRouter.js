1f3ba2f69e58b64915eb90ec1c3ff069a73a3e6d
강한길 <59948163+gksrlf@users.noreply.github.com>
2022-09-17 16:32:32 +0900

    aiServer작업완료

--- routes/qnaRouter.js
+++ routes/qnaRouter.js
@@ -5,4 +5,6 @@ const qnaController = require("../controller/qnaController")
 
 router.get("/", qnaCache.getQnaListCache, qnaController.getQnaListController)
 
+router.post("/text", qnaCache.getAiCache, qnaController.getAiController)
+
 module.exports = router
